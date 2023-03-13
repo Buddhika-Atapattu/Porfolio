5d17a54161a717b60473b74388b283b4777c1787
Buddhika Atapattu <atapattu.buddhika@gmail.com>
2023-03-14 01:27:27 +0400

    add new perloader

--- JS/load.js
+++ JS/load.js
@@ -1,4 +1,22 @@
+let count = 0;
+let preload = Pormise.resolve();
+let fn = (perc) => {
+    p = p.then(() => new Promise(resolve => $("#preloadText").text(perc + "%").delay(200).fadeIn("slow", resolve)));
+};
+while (count < 100) {
+    fn(count + 1);
+    count++;
+}
+
+let start = new Date().getTime();
+
 $(window).on("load", function () {
+
+
+    let now = new Date().getTime();
+    var latency = now - start;
+    // alert("page loading time: " + latency);
+
     let load = $("#preloader");
 
     load.css({ "display": "none" });
